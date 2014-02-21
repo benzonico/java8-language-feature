54bce64033d2465b50b48cba538e1215beeb58b9
Nicolas PERU <nicolas.peru@sonarsource.com>
2014-02-21 11:16:32 +0100

    complete lambda forms

--- src/main/java/LambdaExpressions.java
+++ src/main/java/LambdaExpressions.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.util.concurrent.Callable;
 import java.util.stream.IntStream;
 
@@ -7,9 +8,27 @@ public class LambdaExpressions {
 
     boolean flag = true;
     Callable<Integer> c = flag ? () -> 23 : () -> 42;
-    IntStream.range(1, 5).map((x)->x*x).forEach(System.out::println);
+    IntStream.range(1, 5).map(x->x*x).forEach(System.out::println);
     IntStream.range(1, 5).map((x)-> { return x*x-1; } ).forEach(System.out::println);
 
+    IntStream.range(1,5).mapToObj(x-> new PairSquare(x,x*x)).forEach(System.out::println);
 
+    IntStream.range(1,5).mapToObj((@Foo int a)-> new PairSquare(a,a*a)).forEach(System.out::println);
+    IntStream.range(1,5).mapToObj((int a)-> {return a-a;}).forEach(System.out::println);
+
+  }
+
+  static class PairSquare {
+    int value;
+    int square;
+    PairSquare(int value, int square) {
+      this.square = square;
+      this.value = value;
+    }
+
+    @Override
+    public String toString() {
+      return value+":"+square;
+    }
   }
 }
